d5fa6936d986238e3667fc9ab3e365104adbd4b5
谢嘉晖 <xiejh@scf56.com>
2019-08-05 11:07:20 +0800

    添加项目

---
+++ WindowsFormsForLeecode/WindowsFormsForLeecode/LeeCode/No.1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsForLeecode.LeeCode
+{
+    class No1
+    {
+        public class Solution
+        {
+            public int[] TwoSum(int[] nums, int target)
+            {
+                List<int> resultList = new List<int>();
+                if (nums.Length > 0)
+                {
+                    int len = nums.Length;
+                    for (int i = 0; i < len - 1; i++)
+                    {
+                        for (int j = (i + 1); j < len; j++)
+                        {
+                            if (nums[i] + nums[j] == target)
+                            {
+                                resultList.Add(i);
+                                resultList.Add(j);
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                return resultList.ToArray();
+            }
+        }
+    }
+}
